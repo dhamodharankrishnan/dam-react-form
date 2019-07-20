0f8b7c84a999ab38453a67faacf87b8f49ade130
Dhamodharan Krishnan <dhamodharan.krishnan@walgreens.com>
2019-07-20 14:09:21 -0400

    Patch value

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -11,17 +11,48 @@ export class ProfileEditorComponent implements OnInit {
   formModelValue = '';
   profileForm = new FormGroup( {
     firstName: new FormControl(''),
-    lastName: new FormControl('')
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      zip: new FormControl()
+    })
   });
 
   constructor() { }
 
   ngOnInit() {
+
+    // To set values for all the controls in the form.
+    this.profileForm.setValue(
+      {
+      firstName: 'Dhamo',
+      lastName: 'Krish',
+      address: { street: 'Balaji', city: 'Perumalagaram', state: 'Karnataka', zip: 56001}
+    }
+    );
+
+    // To set individual value in a form.
+    this.profileForm.controls.firstName.setValue('Suresh');
+
+
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
     this.formModelValue = JSON.stringify(this.profileForm.value);
+  }
+
+  // Patch Value example
+  updateProfile() {
+    this.profileForm.patchValue({
+        firstName: 'Savithri',
+        address: {
+          city: 'Canton',
+          zip: '02021'
+        }
+    });
   }
 }
